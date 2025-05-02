78e936b1bcecbd0cd47fc0e6262e8c4756894e8c
tarsis <159029529+TarsisViana@users.noreply.github.com>
2025-05-02 17:28:15 +0100

    erase unecessary files

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -2,11 +2,6 @@
 
 import { getImageList } from "./loader"
 
-export async function createLabel(formData: FormData) {
-  console.log(formData)
-  console.log('working')
-}
-
 export async function getFolderPath(path: string) {
   
   const fileList = await getImageList(path)
